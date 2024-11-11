136dc11adb1e1f3529788c6bfb313f45c257bd55
Saman <samangithoseinpour@gmail.com>
2024-11-11 17:37:35 +0330

    fix: navbar menu issue

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -20,12 +20,12 @@ const NavMenu = () => {
             animate={{ scale: 1, opacity: 1 }}
             exit={{ scale: 0, opacity: 0 }}
             transition={{ duration: 0.5, type: 'spring' }}
-            className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-neutral-50 p-8 z-10 origin-top-right flex flex-col"
+            className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-neutral-50/90 backdrop-blur-xl p-8 z-10 origin-top-right flex flex-col"
           >
             <ul className="flex flex-col gap-2 flex-1">
-              {navItems.map(({ label }) => (
-                <Link href="/" key={label}>
-                  <li className="text-2xl font-medium">{label}</li>
+              {navItems.map((item) => (
+                <Link href={item.href} key={item.id}>
+                  <li className="text-2xl font-medium">{item.label}</li>
                 </Link>
               ))}
             </ul>
