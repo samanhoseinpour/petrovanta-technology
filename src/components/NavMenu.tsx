d2aa6ff6df92b3e881d4e84b93e66f5ed4b248c6
Saman <samangithoseinpour@gmail.com>
2024-11-11 23:34:59 +0330

    fix: nav ui issue

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -20,26 +20,70 @@ const NavMenu = () => {
             animate={{ scale: 1, opacity: 1 }}
             exit={{ scale: 0, opacity: 0 }}
             transition={{ duration: 0.5, type: 'spring' }}
-            className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-neutral-50/90 backdrop-blur-xl p-8 z-10 origin-top-right flex flex-col"
+            className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-[#fefeec48] backdrop-blur-2xl p-8 z-10 origin-top-right flex flex-col"
           >
             <ul className="flex flex-col gap-2 flex-1">
               {navItems.map((item) => (
                 <Link href={item.href} key={item.id}>
-                  <li className="text-2xl font-medium dark:text-[--background]">
+                  <li className="text-2xl font-medium dark:text-[--foreground]">
                     {item.label}
                   </li>
                 </Link>
               ))}
             </ul>
-            <div className="flex gap-4 mt-8">
-              <Link href="/">
-                {/* <Github className="h-5 w-5 fill-github" /> */}
+            <div className="flex gap-4 mt-8 dark:text-[--foreground]">
+              <Link href="/services">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
               </Link>
               <Link href="/">
-                {/* <Dribbble className="h-5 w-5 fill-dribbble" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525"
+                  />
+                </svg>
               </Link>
               <Link href="/">
-                {/* <Twitter className="h-5 w-5 fill-twitter" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z"
+                  />
+                </svg>
               </Link>
             </div>
           </motion.div>
